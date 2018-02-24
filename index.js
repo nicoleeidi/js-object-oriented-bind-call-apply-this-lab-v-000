f275ee78f7638e82a7150f757f2c7b521d2c60ba
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-24 22:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function justInvoke(fn){
 function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue,arg)
 }
-setThisWithApply(fn, thisValue, args){
+function setThisWithApply(fn, thisValue, args){
   return fn.apply(thisValue,[args])
+}
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  return functionToBeCopied.bind(thisValue)
 }
