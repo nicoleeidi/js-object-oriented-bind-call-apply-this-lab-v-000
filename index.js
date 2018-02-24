010283bba136d743018ce7c598e2a26ba9efcfc3
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-24 23:18:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue,arg)
 }
 function setThisWithApply(fn, thisValue, args){
-  return fn.apply(thisValue,[args])
+  
+  return fn.apply(thisValue,args)
 }
 function returnNewFunctionOf(functionToBeCopied, thisValue){
   return functionToBeCopied.bind(thisValue)
